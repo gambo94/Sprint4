aff08bd09685b558e720d286610b1d147b95515d
gambo94 <fedigambo94@gmail.com>
2021-03-22 18:22:08 +0100

    added feature to render html pages with hbs

--- NodeREST_Server/server.js
+++ NodeREST_Server/server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+
+// view engine
 const hbs = require('hbs');
+hbs.registerPartials(__dirname + 'views/partials', function(err){});
+app.set('view engine', 'hbs');
+app.set('views', __dirname + "/views");
 
+// routes
+app.get('/index', (req, res) =>{
+    res.render('index', {
+        title: 'cool title'
+    });
+})
 
+app.get('/about', (req, res) =>{
+    res.render('about');
+})
 
 
 
