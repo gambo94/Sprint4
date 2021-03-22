aac1c41b0259a301df828c7c8f6e679bc003e0b1
gambo94 <fedigambo94@gmail.com>
2021-03-22 17:00:46 +0100

    uploading a litte modification

--- NodeREST_Server/server.js
+++ NodeREST_Server/server.js
@@ -1,25 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const hbs = require('hbs');
 
 
-app.get('/user', ((req, res) =>{
-    res.json(
-        {
-            name: 'Federico',
-            age: 26,
-            url: req.url
-        }
-    );
-}));
 
-let pathPublic = path.resolve('./public');
-
-// this middleware allows to render on the web the files located in the public folder
-app.use(express.static(pathPublic, {
-    // this second parameter allows to thype the file name without the extension and still display it
-    extensions: ['html']
-}));
 
 
 app.listen(8000, () => console.log('server is running on port 8000'));
